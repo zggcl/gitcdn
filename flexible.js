1e81b72ac8e8828567ad3bd5ef5beeb67ffe2b2d
Charlie L <zggcl@users.noreply.github.com>
2020-04-20 16:19:56 +0800

    Create flexible.js
    Cteate flexible.js

---
+++ flexible.js
@@ -0,0 +1,17 @@
+;(function flexible(window, document) {
+    var docEl = document.documentElement
+
+    function setRemUnit() {
+        var rem = docEl.clientWidth / 24
+        docEl.style.fontSize = rem + 'px'
+    }
+
+    setRemUnit()
+
+    window.addEventListener('resize', setRemUnit)
+    window.addEventListener('pageshow', function (e) {
+        if (e.persisted) {
+            setRemUnit()
+        }
+    })
+})(window, document)
